5a3f497e7aee3feffcadbef1e392502cdb43d1e4
alekspanteli <alekspanteli@rambler.ru>
2023-04-20 13:16:07 +0000

    initital

--- app/page.js
+++ app/page.js
@@ -1,238 +1,178 @@
 export default function Home() {
   return (
     <div className="m-8">
-      {/*
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    This is the "not real theming" demo.
-    Scroll down to the next section 
-    to complete the challenge!
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-*/}
-      <section>
-        <h2 className="text-lg font-semibold text-slate-900">
-          How it should look
-        </h2>
-        <div className="mt-2 flex flex-wrap items-center gap-6">
-          <div className="flex overflow-hidden rounded-md">
-            <div className="grid h-16 w-16 place-items-end bg-base-50 p-1 font-semibold leading-none text-base-900">
-              50
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-base-100 p-1 font-semibold leading-none text-base-900">
-              100
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-base-200 p-1 font-semibold leading-none text-base-900">
-              200
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-base-300 p-1 font-semibold leading-none text-base-900">
-              300
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-base-400 p-1 font-semibold leading-none text-base-900">
-              400
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-base-500 p-1 font-semibold leading-none text-base-50">
-              500
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-base-600 p-1 font-semibold leading-none text-base-50">
-              600
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-base-700 p-1 font-semibold leading-none text-base-50">
-              700
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-base-800 p-1 font-semibold leading-none text-base-50">
-              800
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-base-900 p-1 font-semibold leading-none text-base-50">
-              900
-            </div>
-          </div>
-          <div className="flex overflow-hidden rounded-md">
-            <div className="grid h-16 w-16 place-items-end bg-rainforest-50 p-1 font-semibold leading-none text-rainforest-900">
-              50
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-rainforest-100 p-1 font-semibold leading-none text-rainforest-900">
-              100
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-rainforest-200 p-1 font-semibold leading-none text-rainforest-900">
-              200
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-rainforest-300 p-1 font-semibold leading-none text-rainforest-900">
-              300
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-rainforest-400 p-1 font-semibold leading-none text-rainforest-900">
-              400
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-rainforest-500 p-1 font-semibold leading-none text-rainforest-50">
-              500
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-rainforest-600 p-1 font-semibold leading-none text-rainforest-50">
-              600
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-rainforest-700 p-1 font-semibold leading-none text-rainforest-50">
-              700
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-rainforest-800 p-1 font-semibold leading-none text-rainforest-50">
-              800
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-rainforest-900 p-1 font-semibold leading-none text-rainforest-50">
-              900
-            </div>
-          </div>
-          <div className="flex overflow-hidden rounded-md">
-            <div className="grid h-16 w-16 place-items-end bg-candy-50 p-1 font-semibold leading-none text-candy-900">
-              50
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-candy-100 p-1 font-semibold leading-none text-candy-900">
-              100
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-candy-200 p-1 font-semibold leading-none text-candy-900">
-              200
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-candy-300 p-1 font-semibold leading-none text-candy-900">
-              300
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-candy-400 p-1 font-semibold leading-none text-candy-900">
-              400
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-candy-500 p-1 font-semibold leading-none text-candy-50">
-              500
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-candy-600 p-1 font-semibold leading-none text-candy-50">
-              600
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-candy-700 p-1 font-semibold leading-none text-candy-50">
-              700
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-candy-800 p-1 font-semibold leading-none text-candy-50">
-              800
-            </div>
-            <div className="grid h-16 w-16 place-items-end bg-candy-900 p-1 font-semibold leading-none text-candy-50">
-              900
-            </div>
-          </div>
-        </div>
-      </section>
-
-      {/*
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    🏆 Your challenge
-    
-    Without touching the markup below, 
-    recreate the three color spreads from above.
-
-    Here are the steps to follow:
-
-    - Define CSS variables in the CSS file
-
-    - Extend your colors object in the Tailwind config
-  */}
-      <section className="mt-24">
-        <h2 className="text-lg font-semibold text-slate-900">
-          Don't touch the HTML markup!
-        </h2>
-        <div className="mt-2 flex flex-wrap items-center gap-6">
-          <div className="flex overflow-hidden rounded-md" data-theme="base">
-            <div className="bg-primary-50 text-primary-900 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              50
-            </div>
-            <div className="bg-primary-100 text-primary-900 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              100
-            </div>
-            <div className="bg-primary-200 text-primary-900 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              200
-            </div>
-            <div className="bg-primary-300 text-primary-900 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              300
-            </div>
-            <div className="bg-primary-400 text-primary-900 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              400
-            </div>
-            <div className="bg-primary-500 text-primary-50 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              500
-            </div>
-            <div className="bg-primary-600 text-primary-50 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              600
-            </div>
-            <div className="bg-primary-700 text-primary-50 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              700
-            </div>
-            <div className="bg-primary-800 text-primary-50 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              800
-            </div>
-            <div className="bg-primary-900 text-primary-50 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              900
-            </div>
-          </div>
-          <div
-            className="flex overflow-hidden rounded-md"
-            data-theme="rainforest"
-          >
-            <div className="bg-primary-50 text-primary-900 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              50
-            </div>
-            <div className="bg-primary-100 text-primary-900 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              100
-            </div>
-            <div className="bg-primary-200 text-primary-900 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              200
-            </div>
-            <div className="bg-primary-300 text-primary-900 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              300
-            </div>
-            <div className="bg-primary-400 text-primary-900 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              400
-            </div>
-            <div className="bg-primary-500 text-primary-50 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              500
-            </div>
-            <div className="bg-primary-600 text-primary-50 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              600
-            </div>
-            <div className="bg-primary-700 text-primary-50 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              700
-            </div>
-            <div className="bg-primary-800 text-primary-50 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              800
-            </div>
-            <div className="bg-primary-900 text-primary-50 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              900
-            </div>
-          </div>
-          <div className="flex overflow-hidden rounded-md" data-theme="candy">
-            <div className="bg-primary-50 text-primary-900 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              50
-            </div>
-            <div className="bg-primary-100 text-primary-900 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              100
-            </div>
-            <div className="bg-primary-200 text-primary-900 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              200
-            </div>
-            <div
-              className="bg-primary-300 text-primary-900 grid h-16 w-16 place-items-end p-1 font-semibold leading-none"
-            >
-              300
-            </div>
-            <div className="bg-primary-400 text-primary-900 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              400
-            </div>
-            <div className="bg-primary-500 text-primary-50 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              500
-            </div>
-            <div className="bg-primary-600 text-primary-50 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              600
-            </div>
-            <div className="bg-primary-700 text-primary-50 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              700
-            </div>
-            <div className="bg-primary-800 text-primary-50 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              800
-            </div>
-            <div className="bg-primary-900 text-primary-50 grid h-16 w-16 place-items-end p-1 font-semibold leading-none">
-              900
-            </div>
-          </div>
-        </div>
-      </section>
+  <h2 className="text-lg font-semibold text-slate-900">Default Tailwind colors</h2>
+  <div className="mt-2 grid gap-6">
+    <div className="flex gap-4">
+      <div
+        className="grid h-16 w-16 place-items-end rounded-md bg-teal-600 p-1 font-semibold leading-none text-teal-50 ring-2 ring-inset ring-black/10"
+      >
+        100%
+      </div>
+      <div
+        className="grid h-16 w-16 place-items-end rounded-md bg-teal-600 bg-opacity-70 p-1 font-semibold leading-none text-teal-50 ring-2 ring-inset ring-black/10"
+      >
+        70%
+      </div>
+      <div
+        className="grid h-16 w-16 place-items-end rounded-md bg-teal-600/50 p-1 font-semibold leading-none text-teal-900 ring-2 ring-inset ring-black/10"
+      >
+        50%
+      </div>
+      <div
+        className="grid h-16 w-16 place-items-end rounded-md bg-teal-600 bg-opacity-30 p-1 font-semibold leading-none text-teal-900 ring-2 ring-inset ring-black/10"
+      >
+        30%
+      </div>
+      <div
+        className="grid h-16 w-16 place-items-end rounded-md bg-teal-600/[0.1] p-1 font-semibold leading-none text-teal-900 ring-2 ring-inset ring-black/10"
+      >
+        10%
+      </div>
     </div>
+    <div className="flex gap-4">
+      <div
+        className="grid h-16 w-16 place-items-end rounded-md bg-rose-600 p-1 font-semibold leading-none text-rose-50 ring-2 ring-inset ring-black/10"
+      >
+        100%
+      </div>
+      <div
+        className="grid h-16 w-16 place-items-end rounded-md bg-rose-600 bg-opacity-70 p-1 font-semibold leading-none text-rose-50 ring-2 ring-inset ring-black/10"
+      >
+        70%
+      </div>
+      <div
+        className="grid h-16 w-16 place-items-end rounded-md bg-rose-600/50 p-1 font-semibold leading-none text-rose-900 ring-2 ring-inset ring-black/10"
+      >
+        50%
+      </div>
+      <div
+        className="grid h-16 w-16 place-items-end rounded-md bg-rose-600 bg-opacity-30 p-1 font-semibold leading-none text-rose-900 ring-2 ring-inset ring-black/10"
+      >
+        30%
+      </div>
+      <div
+        className="grid h-16 w-16 place-items-end rounded-md bg-rose-600/[0.1] p-1 font-semibold leading-none text-rose-900 ring-2 ring-inset ring-black/10"
+      >
+        10%
+      </div>
+    </div>
+    <div className="flex gap-4">
+      <div
+        className="grid h-16 w-16 place-items-end rounded-md bg-fuchsia-600 p-1 font-semibold leading-none text-fuchsia-50 ring-2 ring-inset ring-black/10"
+      >
+        100%
+      </div>
+      <div
+        className="grid h-16 w-16 place-items-end rounded-md bg-fuchsia-600 bg-opacity-70 p-1 font-semibold leading-none text-fuchsia-50 ring-2 ring-inset ring-black/10"
+      >
+        70%
+      </div>
+      <div
+        className="grid h-16 w-16 place-items-end rounded-md bg-fuchsia-600/50 p-1 font-semibold leading-none text-fuchsia-900 ring-2 ring-inset ring-black/10"
+      >
+        50%
+      </div>
+      <div
+        className="grid h-16 w-16 place-items-end rounded-md bg-fuchsia-600 bg-opacity-30 p-1 font-semibold leading-none text-fuchsia-900 ring-2 ring-inset ring-black/10"
+      >
+        30%
+      </div>
+      <div
+        className="grid h-16 w-16 place-items-end rounded-md bg-fuchsia-600/[0.1] p-1 font-semibold leading-none text-fuchsia-900 ring-2 ring-inset ring-black/10"
+      >
+        10%
+      </div>
+    </div>
+  </div>
+
+
+  <div className="mt-16">
+    <h2 className="text-lg font-semibold text-slate-900">Multi-theme colors</h2>
+    <div className="mt-2 grid gap-6">
+      <div className="flex gap-4" data-theme="rose">
+        <div
+          className="grid h-16 w-16 place-items-end rounded-md bg-primary-600 p-1 font-semibold leading-none text-primary-50 ring-2 ring-inset ring-black/10"
+        >
+          100%
+        </div>
+        <div
+          className="grid h-16 w-16 place-items-end rounded-md bg-primary-600 bg-opacity-70 p-1 font-semibold leading-none text-primary-50 ring-2 ring-inset ring-black/10"
+        >
+          70%
+        </div>
+        <div
+          className="bg-primary-600/50 grid h-16 w-16 place-items-end rounded-md p-1 font-semibold leading-none text-primary-900 ring-2 ring-inset ring-black/10"
+        >
+          50%
+        </div>
+        <div
+          className="grid h-16 w-16 place-items-end rounded-md bg-primary-600 bg-opacity-30 p-1 font-semibold leading-none text-primary-900 ring-2 ring-inset ring-black/10"
+        >
+          30%
+        </div>
+        <div
+          className="bg-primary-600/[0.1] grid h-16 w-16 place-items-end rounded-md p-1 font-semibold leading-none text-primary-900 ring-2 ring-inset ring-black/10"
+        >
+          10%
+        </div>
+      </div>
+      <div className="flex gap-4" data-theme="rainforest">
+        <div
+          className="grid h-16 w-16 place-items-end rounded-md bg-primary-600 p-1 font-semibold leading-none text-primary-50 ring-2 ring-inset ring-black/10"
+        >
+          100%
+        </div>
+        <div
+          className="grid h-16 w-16 place-items-end rounded-md bg-primary-600 bg-opacity-70 p-1 font-semibold leading-none text-primary-50 ring-2 ring-inset ring-black/10"
+        >
+          70%
+        </div>
+        <div
+          className="bg-primary-600/50 grid h-16 w-16 place-items-end rounded-md p-1 font-semibold leading-none text-primary-900 ring-2 ring-inset ring-black/10"
+        >
+          50%
+        </div>
+        <div
+          className="grid h-16 w-16 place-items-end rounded-md bg-primary-600 bg-opacity-30 p-1 font-semibold leading-none text-primary-900 ring-2 ring-inset ring-black/10"
+        >
+          30%
+        </div>
+        <div
+          className="bg-primary-600/[0.1] grid h-16 w-16 place-items-end rounded-md p-1 font-semibold leading-none text-primary-900 ring-2 ring-inset ring-black/10"
+        >
+          10%
+        </div>
+      </div>
+      <div className="flex gap-4" data-theme="candy">
+        <div
+          className="grid h-16 w-16 place-items-end rounded-md bg-primary-600 p-1 font-semibold leading-none text-primary-50 ring-2 ring-inset ring-black/10"
+        >
+          100%
+        </div>
+        <div
+          className="grid h-16 w-16 place-items-end rounded-md bg-primary-600 bg-opacity-70 p-1 font-semibold leading-none text-primary-50 ring-2 ring-inset ring-black/10"
+        >
+          70%
+        </div>
+        <div
+          className="bg-primary-600/50 grid h-16 w-16 place-items-end rounded-md p-1 font-semibold leading-none text-primary-900 ring-2 ring-inset ring-black/10"
+        >
+          50%
+        </div>
+        <div
+          className="grid h-16 w-16 place-items-end rounded-md bg-primary-600 bg-opacity-30 p-1 font-semibold leading-none text-primary-900 ring-2 ring-inset ring-black/10"
+        >
+          30%
+        </div>
+        <div
+          className="bg-primary-600/[0.1] grid h-16 w-16 place-items-end rounded-md p-1 font-semibold leading-none text-primary-900 ring-2 ring-inset ring-black/10"
+        >
+          10%
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
   );
 }
